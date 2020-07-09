1e0273f9bf6205d3a5f0484a413f436e5229469d
sabarinathan321 <sabarinathan935@gmail.com>
2020-07-09 21:05:25 +0530

    register page

--- DSS/DSS/Controllers/HomeController.cs
+++ DSS/DSS/Controllers/HomeController.cs
@@ -15,15 +15,21 @@ namespace DSS.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
             return View();
         }
     }
