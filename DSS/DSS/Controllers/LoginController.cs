9f72d2362ce94fe7b0932addb8d7660b4f58eb9f
Sabarinathan Ayyappan <sabarinathan935@gmail.com>
2020-07-17 22:43:28 +0530

    Register nad login change

--- DSS/DSS/Controllers/LoginController.cs
+++ DSS/DSS/Controllers/LoginController.cs
@@ -8,6 +8,8 @@ using MySql.Data.MySqlClient;
 using MySql.Data;
 using System.Diagnostics;
 using System.Data;
+using System.Text.RegularExpressions;
+using System.Data.SqlClient;
 
 namespace DSS.Controllers
 {
@@ -27,7 +29,7 @@ namespace DSS.Controllers
             try
             {
                 cmd = connection.CreateCommand();
-                cmd.CommandText = "select * from login WHERE username=@user&&pasword=@pass";
+                cmd.CommandText = "select * from Register WHERE UserId=@user&&Password=@pass";
                 cmd.Parameters.AddWithValue("@user", username);
                 cmd.Parameters.AddWithValue("@pass", password);
                 cmd.ExecuteNonQuery();
@@ -68,21 +70,34 @@ namespace DSS.Controllers
         [HttpPost]
         public ActionResult RegisterValidation(string sponsor_id, string sponsor_name, string fname, string lname, string pass, string cpass, string email, string phone, string aadharno, string panno)
         {
-            MySqlConnection connection = new MySqlConnection("Server=localhost;Database=dss;Uid=sab;Pwd=user;");
+            MySqlConnection connection = new MySqlConnection("Server=localhost;Database=dss;Uid=sab;Pwd=user");
             MySqlCommand cmd;
             connection.Open();
             try
             {
-                //cmdFetch = connection.CreateCommand();
-                //cmdFetch.CommandText = "SELECT UserId FROM register order by UserId DESC";
-                //cmdFetch.ExecuteNonQuery();
-                //MySqlDataReader sqlDataReader = cmdFetch.ExecuteReader();
-                //if (sqlDataReader.HasRows)
-                //{
-                //    var lastRow = sqlDataReader.RecordsAffected;
-                //    Debug.WriteLine(lastRow);
-                //}
                 cmd = connection.CreateCommand();
+                cmd.CommandText = "SELECT UserId FROM register ORDER BY UserId DESC LIMIT 1;";
+                cmd.ExecuteNonQuery();
+                MySqlDataReader sqldatareader = cmd.ExecuteReader();
+                string user = null;
+                while (sqldatareader.Read())
+                {
+                    user = sqldatareader.GetString(0);
+                }
+                if (user == null)
+                {
+                    user = "DSS786000001";
+                } else
+                {
+                    String[] spearator = { "DSS" };
+                    String[] strlist = user.Split(spearator, StringSplitOptions.RemoveEmptyEntries);
+                    int finalVal = Convert.ToInt32(strlist[0]) + 1;
+                    Debug.WriteLine(finalVal);
+                    user = string.Concat("DSS", finalVal);
+                    Debug.WriteLine(user);
+                }
+                connection.Close();
+                connection.Open();
                 //cmd.CommandText = "INSERT INTO Register(RefferalId,RefferalName,FirstName,LastName,Password,ConfirmPassword,Email,PhoneNo)VALUES(\"sd\",\"fdf\",\"dfdsf\",\"dfd\",\"sdf\",\"fdsf\",\"dfd\",\"dfdf\")";
                 cmd.CommandText = "INSERT INTO Register(UserId,RefferalId,RefferalName,FirstName,LastName,Password,Email,PhoneNo,AadharNo,PanNo)VALUES(@uid,@rid,@rname,@fname,@lname,@pass,@email,@pno,@aadhar,@pan)";
                 cmd.Parameters.AddWithValue("@rid", sponsor_id);
@@ -90,26 +105,19 @@ namespace DSS.Controllers
                 cmd.Parameters.AddWithValue("@fname", fname);
                 cmd.Parameters.AddWithValue("@lname", lname);
                 cmd.Parameters.AddWithValue("@pass", pass);
-                cmd.Parameters.AddWithValue("@uid", "cfvdvg");
+                cmd.Parameters.AddWithValue("@uid", user);
                 cmd.Parameters.AddWithValue("@email", email);
                 cmd.Parameters.AddWithValue("@pno", phone);
                 cmd.Parameters.AddWithValue("@aadhar", aadharno);
                 cmd.Parameters.AddWithValue("@pan", panno);
                 cmd.ExecuteNonQuery();
+                connection.Close();
                 return RedirectToAction("Login", "Login");
             }
             catch (Exception)
             {
                 return RedirectToAction("Register", "Login");
             }
-            finally
-            {
-                if (connection.State == ConnectionState.Open)
-                {
-                    connection.Close();
-                }
-            }
-
         }
 
         public ActionResult ForgotPassword()
